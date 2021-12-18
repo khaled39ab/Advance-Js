0ecd63fd639d3424fd6d1ac6706f25583cb20eee
khaled39ab <khaled39ab@gmail.com>
2021-12-18 17:03:56 +0600

    Double vs triple equal cleared

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,62 @@
+// const first = 2;
+// const second = 3;
+// if(first == second){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is false");
+// }
+// //Result is : Condition is false
+
+// const first = 2;
+// const second = "2";
+// if(first == second){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is false");
+// }
+// //Result is : Condition is true 
+
+// const first = 2;
+// const second = "2";
+// if(first === second){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is false");
+// }
+// //Result is : Condition is false
+
+// const first = 1;
+// const second = true;
+// if(first == second){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is false");
+// }
+// //Result is : Condition is true
+
+// const first = 1;
+// const second = true;
+// if(first === second){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is false");
+// }
+// //Result is : Condition is false
+
+// const first = 0;
+// const second = false;
+// if(first == second){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is false");
+// }
+// //Result is : Condition is true
+
+const first = 0;
+const second = false;
+if(first === second){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
+//Result is : Condition is false
