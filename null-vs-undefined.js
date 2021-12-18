b9cbb4917263e31c27e247953e3ed53fca6b958f
khaled39ab <khaled39ab@gmail.com>
2021-12-18 16:50:11 +0600

    Null vs Undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,34 @@
+// // how to get undefined
+
+// let people = undefined;
+// console.log(people);
+
+// let people2;
+// console.log(people2);
+
+// function add (){
+//     console.log("Print");
+// }
+// const result = add();
+// console.log(result);
+
+// function add2 (num1, num2){
+//     const sum = num1 + num2;
+//     return;
+// }
+// const result2 = add2();
+// console.log(result2);
+
+// function add3 (num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const result3 = add3(12);
+// console.log(result3);
+
+// const student = {id: "10", name: "Monu"};
+// console.log(student.phone);
+
+/*In JavaScript, undefined is a type, whereas null an object.
+ It means a variable declared, but no value has been assigned a value. 
+ Whereas, null in JavaScript is an assignment value. You can assign it to a variable.*/
